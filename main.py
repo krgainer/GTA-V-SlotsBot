739507c0617d44291f4354ba9882c9974fa75728
Kieran <86863603+krgainer@users.noreply.github.com>
2023-03-27 06:12:11 -0400

    added CSV support for machine data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyautogui
 import ctypes
 import time
+import csv
 import pytesseract
 from PIL import Image
 import re
@@ -14,29 +15,48 @@ consecutiveSpins = 10
 
 ## Unit Variables
 currentGame = ""
-startingBal = 0
-currentBal = 0
+startingChips = 000
+currentChips = 0
 currentProfit = 0
-
+minBet = 0
+maxBet = 0
+betInterval = 0
 
 def main():
 	time.sleep(5)
-	print(resolveChips())
+	currentChips = resolveChips()
+	print(currentChips)
+	gamerules = identifyGameRules("diamond miner")
+	minBet = gamerules[1]
+	maxBet = gamerules[2]
+	betInterval = gamerules[3]
+	print(gamerules)
 	
 
-def currentGame(gameName):
+def identifyGameRules(gameName):
 	gameName = gameName.lower()
-
-
+	with open('machineData.csv') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		line_count = 0
+		gameFound = False
+		for row in csv_reader:
+			if row[0] == gameName:
+				print(f'\t{row[0]} has a minimum bet of {row[1]} with a maximum bet of {row[2]}, going up in intervals of {row[3]}.')
+				gameFound = True
+				return row
+		if gameFound == False:
+			print(f'Game {gameName} not found.')
+			return False
 
 def resolveChips():
+	global startingChips
 	# Determine the number of chips
 	# Return the number of chips
 	screenshot = pyautogui.screenshot("currentBal.png",region=(1464,0, 450, 62))
 	currentBalRaw = pytesseract.image_to_string(Image.open('currentBal.png'), lang='eng')
 	currentBal = re.sub('[^0-9]','', currentBalRaw)
-	if startingBal == 0:
-		startingBal = currentBal
+	if startingChips == 0:
+		startingChips = currentBal
 	return currentBal
 
 
